5c6a56e68fdfa933eb7fb0b8a119a2e4c4200ac6
Unknown Developer <olegis019@gmail.com>
2026-06-08 11:40:36 +0400

    Update sv_resources.lua

--- gamemodes/morbus/gamemode/server/sv_resources.lua
+++ gamemodes/morbus/gamemode/server/sv_resources.lua
@@ -1,28 +1,30 @@
-﻿-- Morbus - morbus.remscar.com
--- Developed by Remscar and the Morbus dev team
-resource.AddWorkshop("3732940164")   -- Morbus Reborn
-resource.AddWorkshop("527017747")    -- Morbus Weapon Content
-resource.AddWorkshop("527020643")    -- Morbus Misc Content
-resource.AddWorkshop("528388523")    -- XMP Morbus Server Content
-resource.AddWorkshop("123295829")    -- Mass Effect Weapons Content
-resource.AddWorkshop("658248292")    -- facility
-resource.AddWorkshop("3629289200")   -- halcyon
-resource.AddWorkshop("186357825")    -- ptmc
-resource.AddWorkshop("282458169")    -- temple
-resource.AddWorkshop("187891235")    -- alphastation
-resource.AddWorkshop("187895089")    -- installation
-resource.AddWorkshop("658240191")    -- breach
-resource.AddWorkshop("272895564")    -- breach_cv
-resource.AddWorkshop("658240507")    -- chemical labs
-resource.AddWorkshop("3629292363")   -- grem
-resource.AddWorkshop("658249952")    -- isolation
-resource.AddWorkshop("272900384")    -- isolation_cv
-resource.AddWorkshop("2182734625")   -- outpost
-resource.AddWorkshop("658267074")    -- spaceship
-resource.AddWorkshop("3034931363")   -- station_v2
-resource.AddWorkshop("524166998")    -- turbatio
-resource.AddWorkshop("658250566")    -- skandlathon
-resource.AddWorkshop("305439293")    -- UT2k4 Robot Playermodels
+﻿resource.AddWorkshop( "3732940164" ) -- Morbus Reborn
+resource.AddWorkshop( "527017747" )  -- Morbus Weapon Content
+resource.AddWorkshop( "527020643" )  -- Morbus Misc Content
+
+resource.AddWorkshop( "528388523" )  -- XMP Morbus Server Content
+resource.AddWorkshop( "123295829" )  -- Mass Effect Weapons Content
+
+resource.AddWorkshop( "658248292" )  -- facility
+resource.AddWorkshop( "3629289200" ) -- halcyon
+resource.AddWorkshop( "186357825" )  -- ptmc
+resource.AddWorkshop( "282458169" )  -- temple
+resource.AddWorkshop( "187891235" )  -- alphastation
+resource.AddWorkshop( "187895089" )  -- installation
+resource.AddWorkshop( "658240191" )  -- breach
+resource.AddWorkshop( "272895564" )  -- breach_cv
+resource.AddWorkshop( "658240507" )  -- chemical labs
+resource.AddWorkshop( "3629292363" ) -- grem
+resource.AddWorkshop( "658249952" )  -- isolation
+resource.AddWorkshop( "272900384" )  -- isolation_cv
+resource.AddWorkshop( "2182734625" ) -- outpost
+resource.AddWorkshop( "658267074" )  -- spaceship
+resource.AddWorkshop( "3034931363" ) -- station_v2
+resource.AddWorkshop( "524166998" )  -- turbatio
+resource.AddWorkshop( "658250566" )  -- skandlathon
+
+resource.AddWorkshop( "305439293" )  -- UT2k4 Robot Playermodels
+
 -- XMP Morbus Content adds itself via addon
 -- Mass Effect Weapons content adds itself via addon
 -- UT4 Robot Playermodels adds itself via addon
